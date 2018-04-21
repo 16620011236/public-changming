f4a6bdff12093a2d5a98201937d88aa78c6a61de
changming <1103359338@qq.com>
2018-04-21 13:17:36 +0800

    version-2.0

---
+++ com/changming/view/MyAction.java
@@ -0,0 +1,12 @@
+啊飒飒的多个放 package com.changming.view;
+
+// version-2.0
+public class MyAction {
+	public static void main(String[] args){
+		String sname="旺财";
+		System.out.println("Hello World:");
+	}
+
+
+
+}

cb6b63d78b734244d9e3662a4d2418e9a531fbd2
changming <1103359338@qq.com>
2018-04-21 18:07:01 +0800

    this is java projck

---
+++ com/changming/view/MyAction.java
@@ -0,0 +1,16 @@
+package com.changming.view;
+
+// version-2.0
+public class MyAction {
+	
+	public static void main(String[] args){
+		
+		String sname="旺财";
+		
+		System.out.println("Hello World:");
+		
+	}
+
+
+
+}

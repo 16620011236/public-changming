e663868cf729491ab8db35dde307036f3201417e
changming <1103359338@qq.com>
2018-04-21 17:56:15 +0800

    this is java en-ch

---
+++ com/changming/view/MyAction.java
@@ -0,0 +1,16 @@
+package com.changming.view;
+
+// version-2.0
+public class MyAction {
+	士大夫立刻地方和高科技白癜风g
+	public static void main(String[] args){
+		电饭锅打开开关灰色空间的吧
+		String sname="旺财";
+		阿斯捷控股斯达康警方共接待高峰电饭锅
+		System.out.println("Hello World:");
+		
+	}
+
+
+
+}

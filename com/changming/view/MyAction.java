de906f4b1bfca030fe77cda28cf3ee014bdadee0
changming <1103359338@qq.com>
2018-04-22 13:26:03 +0800

    master-1.0

--- com/changming/view/MyAction.java
+++ com/changming/view/MyAction.java
@@ -3,15 +3,7 @@ package com.changming.view;
 // version-2.0
 public class MyAction {
 	
-	public void first(){
-		
-	}
-	
-	public static void main(String[] args){
-		
-		String sname="旺财";
-		
-		System.out.println("Hello World:");
+	public void master(){
 		
 	}
 

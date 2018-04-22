6e9b74dc1f108b4503373c12dfc3f8e16c5c0751
changming <1103359338@qq.com>
2018-04-22 16:14:34 +0800

    this is first

--- com/changming/view/MyAction.java
+++ com/changming/view/MyAction.java
@@ -3,8 +3,8 @@ package com.changming.view;
 // version-2.0
 public class MyAction {
 
-	public void  master(){
-		System.out.println("this is master");
+	public void  first(){
+		System.out.println("this is first");
 	}
 }
 
